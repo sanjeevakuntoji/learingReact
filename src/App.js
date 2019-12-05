497fda8343e3d6bdc222980e257cb10cd9419565
Sanjeeva Vasantha Kuntoji <Sanjeeva.Kuntoji@Ltts.com>
2019-12-05 18:21:27 +0530

    'Created_commentDetails_and_ApprovalCard_components'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 import faker from 'faker';
+import CommentDetails from './CommentDetails';
+import ApprovalCard from './ApprovalCard'
 
-
+/////////Buildeing content with jsx///////////
 // function App() {
 //   // const submit = 'Click me!';
 //      const submit = { text:'Click me!'}
@@ -19,53 +21,44 @@ import faker from 'faker';
 //   );
 // }
 
-
+//////////////communication with prpos////////////
 function App() {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar"  src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="auther">
-            sam
-          </a>
-          <div className="metadata">
-            <span className="date">today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar"  src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="auther">
-            sam
-          </a>
-          <div className="metadata">
-            <span className="date">today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar"  src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="auther">
-            sam
-          </a>
-          <div className="metadata">
-            <span className="date">today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <ApprovalCard>
+        <CommentDetails
+          author="Sam"
+          timeAgo="Today At 4.45PM"
+          info="Nice post"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Alex"
+          timeAgo="Today At 6.05PM"
+          info="I like the subject"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>    <ApprovalCard>
+        <CommentDetails
+          author="Jane"
+          timeAgo="Today At 12.35PM"
+          info=" I like writing"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+
     </div>
   )
 }
+
+///////////classbased component//////////
+// function App () {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   )
+//   return (
+//     <div>HI there</div>
+//   )
+// }
 export default App;
